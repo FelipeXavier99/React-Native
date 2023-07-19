97fd7fed24367963da55ae612b576f5b6db8d964
Felipe Xavier <felipe.cvxavier@gmail.com>
2023-07-19 18:07:35 -0300

    Inicio Campo Minado

--- Campo Minado/Minado/App.js
+++ Campo Minado/Minado/App.js
@@ -1,8 +1,11 @@
 import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
 
+// You can import from local files
+import AssetExample from './components/AssetExample';
 
-
-
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-paper';
 
 export default function App() {
   return (
@@ -10,7 +13,9 @@ export default function App() {
       <Text style={styles.paragraph}>
       Xavier
       </Text>
-      
+      <Card>
+        <AssetExample />
+      </Card>
     </View>
   );
 }
@@ -20,6 +25,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
     backgroundColor: '#ecf0f1',
     padding: 8,
   },
